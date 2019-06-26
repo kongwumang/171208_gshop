3a403a2eb805aeefb57bbfeaeeff6a6616f5e3b5
kongwumang <782643273@qq.com>
2019-06-26 23:17:45 +0800

    2019062 no new update

---
+++ src/router/index.js
@@ -0,0 +1,54 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import MSite from '@/pages/MSite/MSite.vue'
+import Order from '@/pages/Order/Order.vue'
+import Profile from '@/pages/Profile/Profile.vue'
+import Search from '@/pages/Search/Search.vue'
+import Login from '@/pages/Login/Login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [{
+      path: '/msite',
+      name: 'MSite',
+      component: MSite,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/order',
+      name: 'Order',
+      component: Order,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/profile',
+      name: 'Profile',
+      component: Profile,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/search',
+      name: 'Search',
+      component: Search,
+      meta: {
+        showFooter: true
+      }
+    },
+    {
+      path: '/',
+      redirect: 'MSite'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    }
+  ]
+})
